8a78216bb36b9ecd2b5caa02bb2d7a35ae7556f1
ISND 6to ISND67 7mo Semestre <ISND67@AISND20-22.campus.iest.edu.mx>
2023-03-30 15:39:02 -0600

    Commit

--- assets/game.js
+++ assets/game.js
@@ -50,7 +50,7 @@ const generateGame = () => {
         throw new Error("The dimension of the board must be an even number.")
     }
 
-    const emojis = ['🥔', '🍒', '🥑', '🌽', '🥕', '🍇', '🍉', '🍌', '🥭', '🍍']
+    const emojis = ['👻', '🤡', '🦨', '🐈', '🐍', '🕷', '🥴', '💀', '👽', '🐊', '🥵', '😴', '🤯','💩', '😼', '😵','🦝', '🤕']
     const picks = pickRandom(emojis, (dimensions * dimensions) / 2) 
     const items = shuffle([...picks, ...picks])
     const cards = `
